45d17e9f16ee64f03bf077552917718cc8be5e1f
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-03-06 14:01:07 -0300

    romovendo catch de erro

--- src/controllers/AddressController.ts
+++ src/controllers/AddressController.ts
@@ -2,72 +2,60 @@ import { Request, Response } from "express";
 import { prisma } from "../database/prisma";
 
 export const createAddress = async (req: Request, res: Response) => {
-  try {
-    const { street, city, neighborhood, CEP, state } = req.body;
-    const { id } = req.user;
+  const { street, city, neighborhood, CEP, state } = req.body;
+  const { id } = req.user;
 
-    const address = await prisma.address.create({
-      data: {
-        street,
-        city,
-        neighborhood,
-        CEP,
-        state,
-        userId: id,
-      },
-    });
+  const address = await prisma.address.create({
+    data: {
+      street,
+      city,
+      neighborhood,
+      CEP,
+      state,
+      userId: id,
+    },
+  });
 
-    return res.status(201).json(address);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
+  return res.status(201).json(address);
 };
 
 export const getAddress = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.user;
+  const { id } = req.user;
 
-    const address = await prisma.address.findMany({
-      where: {
-        userId: id,
-      },
-    });
+  const address = await prisma.address.findMany({
+    where: {
+      userId: id,
+    },
+  });
 
-    return res.status(200).json(address);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
+  return res.status(200).json(address);
 };
 
 export const deleteAddress = async (req: Request, res: Response) => {
-  try {
-    const { addressId } = req.params;
-    const { id } = req.user;
+  const { addressId } = req.params;
+  const { id } = req.user;
 
-    const address = await prisma.address.findUnique({
-      where: {
-        id: addressId,
-      },
-    });
+  const address = await prisma.address.findUnique({
+    where: {
+      id: addressId,
+    },
+  });
 
-    if (!address) {
-      return res.status(404).json({ message: "Endereço não encontrado." });
-    }
-
-    if (address.userId !== id) {
-      return res
-        .status(400)
-        .json({ message: "Endereço não partence a esse usuário." });
-    }
-
-    await prisma.address.delete({
-      where: {
-        id: addressId,
-      },
-    });
-
-    return res.status(200).send();
-  } catch (error) {
-    return res.status(500).json(error);
+  if (!address) {
+    return res.status(404).json({ message: "Endereço não encontrado." });
   }
+
+  if (address.userId !== id) {
+    return res
+      .status(400)
+      .json({ message: "Endereço não partence a esse usuário." });
+  }
+
+  await prisma.address.delete({
+    where: {
+      id: addressId,
+    },
+  });
+
+  return res.status(200).send();
 };
